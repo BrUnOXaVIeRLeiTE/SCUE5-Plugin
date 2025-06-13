c6a746c625405c17b7cc7f34d53bf4eaae3bc9b6
KHK <hokkaido.hideout@gmail.com>
2025-06-13 12:05:03 +0900

    UE5.6
    Updated to UE5.6

--- Plugins/SCUE5/Source/SCUE5/SCUE5.Build.cs
+++ Plugins/SCUE5/Source/SCUE5/SCUE5.Build.cs
@@ -1,24 +1,28 @@
 using UnrealBuildTool;
 using System.IO;
 
-public class SCUE5 : ModuleRules {
-	public SCUE5(ReadOnlyTargetRules Target) : base(Target) {
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+public class SCUE5 : ModuleRules
+{
+	public SCUE5(ReadOnlyTargetRules Target) : base(Target)
+	{
 		PrivatePCHHeaderFile = "Private/SCUE5PrivatePCH.h";
-		bEnforceIWYU = true;
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		IWYUSupport = IWYUSupport.None;
 		//
-		PublicDependencyModuleNames.AddRange(
-			new string[] {
+		PublicDependencyModuleNames.AddRange
+		(
+			new string[]
+			{
 				"Core",
 				"Engine",
 				"SCUE5X",
 				"Projects",
 				"CoreUObject"
-			}///
-		);//
+			}
+		);
 		//
 		PublicDefinitions.Add("WITH_ANTI_CHEAT");
 		PublicIncludePaths.Add(Path.Combine(ModuleDirectory,"Public"));
 		PrivateIncludePaths.Add(Path.Combine(ModuleDirectory,"Private"));
-	}///
+	}
 }
